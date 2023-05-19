c2edcaaffc81ee4c99e67d7b5749f80f19839d52
Jeewoo Kim <jeewoo1998@gmail.com>
2023-05-19 21:01:02 +0100

    Copy and pasted option-critic codebases.

---
+++ option_critic.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+from torch.distributions import Categorical, Bernoulli
+
+from math import exp
+import numpy as np
+
+from utils import to_tensor
+
+
+class OptionCritic(nn.Module):
+    def __init__(self,
+                in_features,
+                num_actions,
+                num_options,
+                temperature=1.0,
+                eps_start=1.0,
+                eps_min=0.1,
+                eps_decay=int(1e6),
+                eps_test=0.05,
+                device='cpu',
+                is_eval=False):
+        super(OptionCriticConv, self).__init__()
+
+        self.in_channels = in_features
+        self.num_actions = num_actions
+        self.num_options = num_options
+        self.device = device
+        self.is_eval = is_eval 
+
+        self.temperature = temperature
+        self.eps_min   = eps_min
+        self.eps_start = eps_start
+        self.eps_decay = eps_decay
+        self.eps_test  = eps_test
+        self.num_steps = 0
+        
+        self.features = nn.Sequential(
+            nn.Conv2d(self.in_channels, 16, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.modules.Flatten(),
+        )
+
+        self.Q = nn.Linear(64, num_options) # Policy-Over-Options
+        self.terminations = nn.Linear(64, num_options) # Option-Termination
+        self.options_W = nn.Parameter(torch.zeros(num_options, 64, num_actions))
+        self.options_b = nn.Parameter(torch.zeros(num_options, num_actions))
+
+        self.to(device)
+        self.train(not testing)
+
+    def get_state(self, obs):
+        if obs.ndim < 4:
+            obs = obs.unsqueeze(0)
+        obs = obs.to(self.device)
+        state = self.features(obs)
+        return state
+
+    def get_Q(self, state):
+        return self.Q(state)
+    
+    def predict_option_termination(self, state, current_option):
+        termination = self.terminations(state)[:, current_option].sigmoid()
+        option_termination = Bernoulli(termination).sample()
+        
+        Q = self.get_Q(state)
+        next_option = Q.argmax(dim=-1)
+        return bool(option_termination.item()), next_option.item()
+    
+    def get_terminations(self, state):
+        return self.terminations(state).sigmoid() 
+
+    def get_action(self, state, option):
+        logits = state.data @ self.options_W[option] + self.options_b[option]
+        action_dist = (logits / self.temperature).softmax(dim=-1)
+        action_dist = Categorical(action_dist)
+
+        action = action_dist.sample()
+        logp = action_dist.log_prob(action)
+        entropy = action_dist.entropy()
+
+        return action.item(), logp, entropy
+    
+    def greedy_option(self, state):
+        Q = self.get_Q(state)
+        return Q.argmax(dim=-1).item()
+
+    @property
+    def epsilon(self):
+        if not self.is_eval:
+            eps = self.eps_min + (self.eps_start - self.eps_min) * exp(-self.num_steps / self.eps_decay)
+            self.num_steps += 1
+        else:
+            eps = self.eps_test
+        return eps
---
+++ utils.py
@@ -0,0 +1,18 @@
+import gym
+import gym_minigrid
+import numpy as np
+import torch
+
+
+def make_env(env_name):
+    env = gym.make(env_name)
+    if 'MiniGrid' in env_name:
+        env = gym_minigrid.wrappers.ImgObsWrapper(env)
+
+    return env
+
+
+def to_tensor(obs):
+    obs = np.asarray(obs)
+    obs = torch.from_numpy(obs).float()
+    return obs
